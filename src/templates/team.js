2998adf3b103e20292784236d8502898c319ac82
Dan Kempton <kemptondan@gmail.com>
2021-05-25 17:06:07 -0400

    adding more data to page template jsx

--- src/templates/team.js
+++ src/templates/team.js
@@ -3,13 +3,19 @@ import { graphql } from "gatsby"
 
 const Team = ({ data }) => {
   return (
-    <div>
-      <h1>{data.team.teamInfo.name}</h1>
-      <p>{data.team.teamInfo.sport}</p>
-      <div>
+    <div className="team">
+      <div className="team__team-info">
+        <h1>{data.team.teamInfo.name}</h1>
+        <h3>{data.team.teamInfo.sport}</h3>
+        <p>{data.team.teamInfo.league}</p>
+      </div>
+      <div className="team__coaches-list">
         {data.team.coaches.map(coach => (
           <p>{coach}</p>
         ))}
+      </div>
+      <div className="team__max-players">
+        <p>Max number of players: {data.team.numberOfPlayers}</p>
       </div>
     </div>
   )
